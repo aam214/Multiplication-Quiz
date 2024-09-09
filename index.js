30ec56ec2e0e3f3a9fa46250a8068ee42d3bef8f
Ariana <152944814+aam214@users.noreply.github.com>
2024-09-09 14:09:18 -0500

    Edited header and body background styling. Added image for background. Organized order on JS.

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const inputNumber = document.getElementById("input");
 const form = document.getElementById("form");
 const scoreBoard = document.getElementById("score");
 
-const number1 = Math.ceil(Math.random() * 10);
-const number2 = Math.ceil(Math.random() * 10);
+const number1 = Math.ceil(Math.random() * 12);
+const number2 = Math.ceil(Math.random() * 12);
+const rightAnswer = number1 * number2;
 
 let score = JSON.parse(localStorage.getItem("score"));
 
@@ -13,7 +14,7 @@ if (!score) {
 }
 scoreBoard.innerHTML =
 `<i class="fa-solid fa-star"></i> Score: ${score}`;
-const rightAnswer = number1 * number2;
+
 
 questionFormat.innerHTML = 
 `What is ${number1} <i class="fa-solid fa-xmark"></i> ${number2}?`
@@ -23,13 +24,13 @@ form.addEventListener("submit", () => {
   const userAnswer = +inputNumber.value;
 if (userAnswer === rightAnswer) {
   score ++;
-  console.log(score);
+  //console.log(score);
   updateStorage()
   alert("Correct!🎉🎊✨🤩");
 
 } else {
   score--;
-  console.log(score);
+  //console.log(score);
   updateStorage()
   alert("Try Again!");
 }
